c3cd7d94e7a5d16164dd488f8aac03f12915af13
Dmitry Ivanov <d.k.ivanov@live.com>
2016-11-23 17:06:22 +0300

    button listener for MainActivity

--- app/src/main/java/com/pupupon/armenianalphabetquiz/MainActivity.java
+++ app/src/main/java/com/pupupon/armenianalphabetquiz/MainActivity.java
@@ -1,15 +1,28 @@
 package com.pupupon.armenianalphabetquiz;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements OnClickListener, Runnable  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public void onClick(View view) {
+
+    }
+
+    public void run() {
 
     }
 }
