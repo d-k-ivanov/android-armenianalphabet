2819753a78f81232c90a451d3ef345da787c4f52
Dmitry Ivanov <d.k.ivanov@live.com>
2016-11-29 12:53:17 +0300

    Button animation, Result anumation

--- app/src/main/java/com/pupupon/armenianalphabetquiz/MainActivity.java
+++ app/src/main/java/com/pupupon/armenianalphabetquiz/MainActivity.java
@@ -1,54 +1,51 @@
 package com.pupupon.armenianalphabetquiz;
 
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener, OnLongClickListener, Runnable  {
+public class MainActivity extends AppCompatActivity implements OnClickListener, OnLongClickListener {
+    // Vars:
+    TextView questionText;
+    Button answerButton1;
+    Button answerButton2;
+    Button answerButton3;
+    Button answerButton4;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
-/*
-        //Demo
-        OnClickListener questionClick = new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                questionText.setText(getResources().getText(R.string.answer4));
-            }
-        };
-        questionText.setOnClickListener(questionClick);
-*/
-
     }
 
     public void init() {
         setContentView(R.layout.activity_main);
         // Question Section
-        final  TextView questionText = (TextView) findViewById(R.id.textQuestion);
+        questionText = (TextView) findViewById(R.id.textQuestion);
         questionText.setOnLongClickListener(this);
 
         // Answer Section
-        final Button answerButton1 = (Button) findViewById(R.id.answer1);
-        final Button answerButton2 = (Button) findViewById(R.id.answer2);
-        final Button answerButton3 = (Button) findViewById(R.id.answer3);
-        final Button answerButton4 = (Button) findViewById(R.id.answer4);
-
+        answerButton1 = (Button) findViewById(R.id.answer1);
+        answerButton2 = (Button) findViewById(R.id.answer2);
+        answerButton3 = (Button) findViewById(R.id.answer3);
+        answerButton4 = (Button) findViewById(R.id.answer4);
         // Init Buttons:
         answerButton1.setOnClickListener(this);
         answerButton2.setOnClickListener(this);
         answerButton3.setOnClickListener(this);
         answerButton4.setOnClickListener(this);
+
         // Result Section:
-        final TextView resultText = (TextView) findViewById(R.id.textResult);
+        resultText = (TextView) findViewById(R.id.textResult);
 
         // Init Result:
         TextViewCompat.setTextAppearance(resultText, R.style.resultSectionHidden);
@@ -59,23 +56,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
 
         Button button = (Button) view;
 
-        button.setBackgroundResource(R.drawable.button_green);
+        //button.setBackgroundResource(R.drawable.button_green);
         //button.setBackgroundResource(R.drawable.button_red);
-        TextViewCompat.setTextAppearance(button, R.style.answerCorrectButton);
+        //TextViewCompat.setTextAppearance(button, R.style.answerCorrectButton);
 
-        final TextView resultText = (TextView) findViewById(R.id.textResult);
+        final Animation bounce = AnimationUtils.loadAnimation(this, R.anim.button_bounce);
+        ButtonBounceInterpolator interpolator = new ButtonBounceInterpolator(0.1, 20);
+        bounce.setInterpolator(interpolator);
+        button.startAnimation(bounce);
+
         TextViewCompat.setTextAppearance(resultText, R.style.resultSection);
-        resultText.setBackgroundColor(ContextCompat.getColor(this, R.color.answerBackground));
+        resultText.setBackgroundColor(ContextCompat.getColor(this, R.color.resultBackground));
+        final Animation slide = AnimationUtils.loadAnimation(this, R.anim.result_slide);
+        resultText.startAnimation(slide);
 
     }
 
     public boolean onLongClick(View view) {
         init();
         return false;
-    }
-
-    public void run() {
-
     }
 
 }
