667bc6b815b54b49aaef04e5a65f1863f46912dc
flemieux <flemieux@deezer.com>
2019-05-29 18:06:47 +0400

    Add storage to pronunciation flag

---
+++ app/src/main/java/com/pupupon/armenianalphabet/Storage.java
@@ -0,0 +1,21 @@
+package com.pupupon.armenianalphabet;
+
+import android.content.Context;
+
+import com.orhanobut.hawk.Hawk;
+
+public class Storage {
+	private static String EASTERN_ARMENIAN = "eastern_armenian";
+
+	public static void init (Context context) {
+		Hawk.init(context).build();
+	}
+
+	public static void setEasternArmenian(boolean bool) {
+		Hawk.put(EASTERN_ARMENIAN, bool);
+	}
+
+	public static boolean getEasternArmenian() {
+		return Hawk.get(EASTERN_ARMENIAN, true);
+	}
+}
