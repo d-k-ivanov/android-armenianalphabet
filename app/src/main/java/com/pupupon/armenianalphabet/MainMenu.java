3a20ce090bc8ed840085609dcca5ae0782414ec9
flemieux <flemieux@deezer.com>
2019-06-13 23:00:52 +0400

    Add analytics to main menu activity

--- app/src/main/java/com/pupupon/armenianalphabet/MainMenu.java
+++ app/src/main/java/com/pupupon/armenianalphabet/MainMenu.java
@@ -3,16 +3,26 @@ package com.pupupon.armenianalphabet;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.URLSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsActivity;
+
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.ACTION_ABOUT;
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.ACTION_ALPHABET;
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.ACTION_GITHUB;
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.ACTION_QUIZ;
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.ACTION_SWITCH_PRONUNCIATION;
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.LABEL_EASTERN;
+import static com.pupupon.armenianalphabet.googleanalytics.GoogleAnalyticsConstants.LABEL_WESTERN;
 
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends GoogleAnalyticsActivity {
     // Vars:
     Button[] buttons = new Button[4];
 
@@ -23,12 +33,12 @@ public class MainMenu extends AppCompatActivity {
         Typeface mainFont = Tools.setFont(this);
 
         // Answer Section
-        buttons[0] = (Button) findViewById(R.id.menuEntry1);
-        buttons[1] = (Button) findViewById(R.id.menuEntry2);
-        buttons[2] = (Button) findViewById(R.id.menuEntry3);
+        buttons[0] = findViewById(R.id.menuEntry1);
+        buttons[1] = findViewById(R.id.menuEntry2);
+        buttons[2] = findViewById(R.id.menuEntry3);
         /*buttons[2] = (Button) findViewById(R.id.menuEntry4);*/
-        buttons[3] = (Button) findViewById(R.id.menuEntry5);
-
+        buttons[3] = findViewById(R.id.menuEntry5);
+        setUrlClickListener((TextView)findViewById(R.id.aboutCopyright));
         // Init Buttons:
         for (Button i : buttons) {
             i.setTypeface(mainFont);
@@ -41,16 +51,19 @@ public class MainMenu extends AppCompatActivity {
     }
 
     public void startMenuEntry1(View view) {
+        userAction(ACTION_ALPHABET);
         Intent alphabet = new Intent(this, AlphabetActivity.class);
         startActivity(alphabet);
     }
 
     public void startMenuEntry2(View view) {
+        userAction(ACTION_QUIZ);
         Intent quiz = new Intent(this, QuizActivity.class);
         startActivity(quiz);
     }
 
     public void startMenuEntry3(View view) {
+        eventSwitchPronunciation();
         Snackbar.make(findViewById(R.id.activity_main_menu),
             getString(getArmenianIndication()) + getString(R.string.enabled),
             Snackbar.LENGTH_SHORT).show();
@@ -60,18 +73,22 @@ public class MainMenu extends AppCompatActivity {
 
     }
 
-    /*public void startMenuEntry4(View view) {
-        Intent help = new Intent(this, HelpActivity.class);
-        startActivity(help);
-    }*/
-
     public void startMenuEntry5(View view) {
+        userAction(ACTION_ABOUT);
         Intent about = new Intent(this, AboutActivity.class);
         startActivity(about);
     }
 
     private int getPronunciationIndication() {
         return Storage.getEasternArmenian() ? R.string.westernPronunciation : R.string.easternPronunciation;
+    }
+
+    private void eventSwitchPronunciation() {
+        if(Storage.getEasternArmenian()) {
+            userAction(ACTION_SWITCH_PRONUNCIATION, LABEL_WESTERN);
+        } else {
+            userAction(ACTION_SWITCH_PRONUNCIATION, LABEL_EASTERN);
+        }
     }
 
     private int getArmenianIndication() {
@@ -81,5 +98,35 @@ public class MainMenu extends AppCompatActivity {
     private void setTittle() {
         String armPrefix = (Storage.getEasternArmenian() ? getString(R.string.eastern) : getString(R.string.western)) + " ";
         this.getSupportActionBar().setTitle(armPrefix + getString(R.string.app_name));
+    }
+
+    private void setUrlClickListener(TextView tv) {
+        SpannableString current = (SpannableString) tv.getText();
+        URLSpan[] spans =
+            current.getSpans(0, current.length(), URLSpan.class);
+
+        for (URLSpan span : spans) {
+            int start = current.getSpanStart(span);
+            int end = current.getSpanEnd(span);
+
+            current.removeSpan(span);
+            current.setSpan(new DefensiveURLSpan(span.getURL()), start, end,
+                0);
+        }
+    }
+
+    public class DefensiveURLSpan extends URLSpan {
+        private String mUrl;
+
+        public DefensiveURLSpan(String url) {
+            super(url);
+            mUrl = url;
+        }
+
+        @Override
+        public void onClick(View widget) {
+            super.onClick(widget);
+            MainMenu.this.userAction(ACTION_GITHUB, mUrl);
+        }
     }
 }
